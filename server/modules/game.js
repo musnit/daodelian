525f5216db9f386d0ebc0c7db480bed5785009da
Trevor Clarke <besnoid@gmail.com>
2019-04-20 18:24:14 +0200

    setup game module

--- server/modules/game.js
+++ server/modules/game.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const redis = require('../lib/redis');
+const db = require('../lib/redis');
 const StateChannel = require('./StateChannel');
 
 class Game {
@@ -10,8 +10,7 @@ class Game {
 
   // Setup all variables needed
   // initialize state channel
-  start(ctx) {
-    // TODO: Use new state channel
+  async start(ctx, next) {
     const { contractAddress } = ctx.body
     const scInstance = new StateChannel({
       address: contractAddress,
@@ -19,25 +18,48 @@ class Game {
     })
 
     const game = {
+      // Required data
       channelId: this.channelId,
+      stateChannel: scInstance,
+      pendingState: [],
+      state: [],
 
+      // Configurations for game specifics
+      options: {
+        // EXAMPLE:
+        // gameType: 'sc2',
+        // strategy: 1
+      }
     }
+    console.log('start game data', this.channelId, game)
 
-
-    await db.setKey('game', this.channelId, game);
-    ctx.body = {
-      success: true,
-    };
+    await db.setKey('game', this.channelId, game)
+    ctx.body = { success: true }
+    next()
   }
 
   // adds proposal to pending game state for a team
-  proposeMove() {
+  async proposeMove({ channelId, proposedState, proposedIdx }) {
+    // TODO: get game by ID, update pending state, store
+    // If majority votes propose same, then commit!
 
+    const game = await db.getKey('game', channelId)
+
+    if (proposedIdx) game.pendingState[proposedIdx](proposedState)
+    else (proposedIdx) game.pendingState.push(proposedState)
+    console.log('proposeMove', game)
+
+    return await db.setKey('game', this.channelId, game)
   }
 
   // adds proposal to game state (redis) and state channel
-  commitProposal() {
-    // TODO: Send to the state channel
+  commitProposal({ channelId, proposedIdx }) {
+    // Get game proposal and Send to the state channel
+    const game = await db.getKey('game', channelId)
+    const proposal = game.pendingState.splice(proposedIdx, 1)
+    game.state.push(proposal)
+    console.log('commitProposal', game)
+    return await db.setKey('game', this.channelId, game)
   }
 }
 
