b662c6253baa229cc74bce5f7503cd04c7b07297
Trevor Clarke <besnoid@gmail.com>
2019-04-20 18:24:14 +0200

    setup game module

--- server/modules/stateChannel.js
+++ server/modules/stateChannel.js
@@ -1,21 +1,38 @@
 const crypto = require('crypto');
+// const redis = require('../lib/redis');
 // const web3Provider = require('../lib/web3Provider');
 
 class StateChannel {
   constructor({ address, channelId, validator, participants }) {
-    this.address = address;
-    this.channelId = channelId;
-    this.validator = validator;
-    this.participants = participants;
+    this.address = address
+    this.channelId = channelId
+    this.validator = validator
+    this.participants = participants
+    this.stage = 'TBD'
+    this.currentTurnNum = 0
+    this.commitHashes = []
+    this.committers = []
+    this.salts = []
 
     return this
   }
 
   stateUpdate() {
-    //
+    const action = {
+      participant: '0x...',
+      turnNum: 0,
+      type: '',
+      salt: '',
+      commitHash: ''
+    }
+
+    // TODO: finish!
+    return this
   }
 
-  stateResolve() {}
+  stateResolve() {
+    // NOTE: Not worrying about this for hackathon
+  }
 }
 
 module.exports = StateChannel;
